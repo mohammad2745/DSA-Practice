ca7c7f33194eca96766554e572a03d7467340701
mohammad2745 <mohammadfcc2745@gmail.com>
2025-12-26 12:37:17 +0600

    Merge two sorted array

---
+++ array-practice/mergeSortedArray.js
@@ -0,0 +1,24 @@
+const merge = (nums1, m, nums2, n) => {
+  let last = m + n -1;
+  
+  while(m > 0 && n >0) {
+    if(nums1[m-1] > nums2[n-1]) {
+      nums1[last] = nums1[m-1];
+      m--;
+    } else {
+      nums1[last] = nums2[n-1];
+      n--;
+    }
+    last--;
+  }
+
+  while(n > 0) {
+    nums1[last] = nums2[n-1];
+    n--;
+    last--;
+  }
+
+  return nums1;
+};
+
+console.log(merge([1], 1, [], 0));
